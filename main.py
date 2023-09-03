2b2a679f16ec0e75e669d84aaaf6eb33b84e8349
reddiedev <red.mendoza@shopee.com>
2023-09-03 14:31:59 +0800

    added db connection and initial loadprefixdata endpoint

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+import psycopg2
 from typing import Union
 from fastapi import FastAPI
+import datetime
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
 
-# TODO: setup database connection
+# database connection
+try:
+    conn = psycopg2.connect(database=os.environ.get('DB_NAME'),
+                            host=os.environ.get('DB_HOST'),
+                            user=os.environ.get('DB_USER'),
+                            port=os.environ.get('DB_PORT'))
+except:
+    print("unable to connect to db")
+
+cursor = conn.cursor()
 
 ALLOWED_CARRIERS = ["globe", "smart", "sun",
                     "dito", "tm", "tnt" "cherry", "gomo"]
@@ -31,8 +45,16 @@ def read_item(prefix: Union[str, None] = None):
 # upsert prefix and carrier data for one pair
 # input: object(prefix, carrier)
 # TODO: update data type
-def read_item(carrier: Union[str, None] = None):
-    return "Hello World!"
+def read_item(prefix: Union[str, None] = None, carrier: Union[str, None] = None):
+    try:
+        current_date = datetime.datetime.now()
+        cursor.execute(
+            "INSERT INTO prefixes(prefix,carrier,created_at,updated_at) VALUES (%s,%s,%s,%s)", (prefix, carrier, current_date, current_date))
+        conn.commit()
+    except Exception as e:
+        print(e)
+        print("error loading prefix data")
+    return "OK!"
 
 
 @app.post("/api/loadPrefixesData")
